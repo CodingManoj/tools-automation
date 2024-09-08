cac0fa5978356fa72b00732993242d468f69f6e0
CodingManoj <CodingManoj@gmail.com>
2024-09-08 01:18:43 -0400

    AutoPush

--- module/main.tf
+++ module/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "main" {
 # Creates DNS Record
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = "${var.name}.expense.internal"
+  name    = "${var.name}s.expense.internal"
   type    = "A"
   ttl     = 10
   records = [aws_instance.main.private_ip]
